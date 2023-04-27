deb45f0362f12e0fe821696edeaef94d31f11efd
KAASIPRASANTH <kaasiprasanth.a2020cse@sece.ac.in>
2023-04-27 12:45:10 +0530

    commit

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
       :event-handlers="eventHandlers"
     >
       <template #override-node="{ nodeId, scale, config, ...slotProps }">
-      
         <circle
           v-bind="slotProps"
           :ref="nodeId"
@@ -46,6 +45,8 @@
        Destination:<input type="number" v-model="destinationVal" required>
        Cost:<input type="number" v-model="Cost" required>
        <el-button @click="addNode"><b>Add</b></el-button>
+       <el-button @click="BFS"><b>BFS</b></el-button>
+       <el-button @click="DFS"><b>DFS</b></el-button>
     </label>
   </div>
 </template>
@@ -66,6 +67,8 @@ export default {
     destinationVal:0,
     counter:11,
     Cost:0,
+    curr:1,
+    edgeCount:0,
     testCaseCondition: false,
   }),
   
@@ -93,7 +96,115 @@ export default {
   },
 
   methods: {
+    BFS(){
+      this.edgeCount=0;
+      //creating data part
+      let vertices = [];
+      let adj = {};
+      for(let v=1;v<=1000;v++){
+        if(nodes[v]!=null){
+          vertices.push(v);
+          adj[v] = [];
+        }
+      }
+      for(let i=1;i<=1000;i++){
+        if(edges[i]!=null){
+          let s=parseInt(edges[i].source);
+          let d=parseInt(edges[i].target);
+          adj[s].push(d);
+        }
+      }
+
+      var visited = {};
+      var r = {};
+      for(let i=1;i<=1000;i++){
+        r[i] = [];
+      }
+      let q = []
+
+      visited[1] = true;
+      q.push(1);
+
+      let flag=1;
+      while(q.length>0){
+        var len = q.length;
+        while(len-->0){
+          var curr = q[0];
+          r[flag].push(curr);
+          q.shift();
+          var list = adj[curr]
+          for(var i in list){
+            var neigh = list[i];
+            if(!visited[neigh]){
+              visited[neigh] = true;
+              q.push(neigh);
+            }
+          }
+        }
+        flag++;
+      }
+
+      
+      for(let f=1;f<=10;f++){
+        var l = r[f];
+        if(f == this.curr){
+          for(var v in l){
+            let source = l[v];
+            this.$refs[source+""].style.strokeWidth = this.size["hover"];
+          }
+        }
+      }
+      
+      this.curr++;
+    },
+    doDFS(curr,adj,visited,res){
+      visited[curr] = true;
+      res.push(curr);
+      var l = adj[curr];
+      for(var v in l){
+        var next = l[v];
+        if(!visited[next]){
+          this.doDFS(next,adj,visited,res);
+        }
+      }
+    },
+    DFS(){
+      this.curr=1;
+      console.log("started");
+      let vertices = [];
+      let adj = {};
+      for(let v=1;v<=1000;v++){
+        if(nodes[v]!=null){
+          vertices.push(v);
+          adj[v] = [];
+        }
+      }
+      for(let i=1;i<=1000;i++){
+        if(edges[i]!=null){
+          let s=parseInt(edges[i].source);
+          let d=parseInt(edges[i].target);
+          adj[s].push(d);
+        }
+      }
+      var visited = {};
+      let res = [];
+      for(let v=0;v<=10;v++){
+        let node = vertices[v];
+        if(!visited[node]){
+          this.doDFS(node,adj,visited,res);
+        }
+      }
+      for(let vertex=0;vertex<res.length;vertex++){
+        if(vertex == this.edgeCount){
+          let source = res[vertex];
+          if(this.nodes[source]) this.$refs[source+""].style.strokeWidth = this.size["hover"];
+        }
+      }
+      this.edgeCount++;
+    },
     addNode(){
+      this.edgeCount=0;
+      this.curr=1;
       if(this.nodeVal) this.nodes[this.nodeVal] = {edgeWidth: 1, hue: 160 };
       if(this.sourceVal && this.destinationVal){
         let s = this.sourceVal.toString();
@@ -103,20 +214,25 @@ export default {
       } 
     },
     handleHoverNode(node, size, color) {
+      this.curr=1;
+      this.edgeCount=0;
     for(let i=1;i<=1000;i++){
       if(edges[i]!=null && (edges[i].source === node || edges[i].target === node)){
         edges[i].edgeWidth = this.size[size];
         const defaultColor = `hsl(${edges[i].hue}, 50%, 50%)`;
-          this.$refs[edges[i].source].style.stroke = color ?? defaultColor;
+          this.$refs[edges[i].source].style.stroke = defaultColor;
           this.$refs[edges[i].source].style.strokeWidth = this.size[size];
 
-          this.$refs[edges[i].target].style.stroke = color ?? defaultColor;
+          this.$refs[edges[i].target].style.stroke = defaultColor;
           this.$refs[edges[i].target].style.strokeWidth = this.size[size];
       }
+      console.log(color);
     }
     },
 
     handleHoverEdge(edge, size, color) {
+      this.curr = 1;
+      this.edgeCount=0;
       const { source, target, hue } = this.edges[edge];
       const defaultColor = `hsl(${hue}, 50%, 50%)`;
 
--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const hues = [200, 320, 80, 160, 0, 240, 120, 280, 40];
 export const size = {
   default: 1,
-  hover: 8
+  hover: 12
 };
 
 export const nodes = {
@@ -20,17 +20,18 @@ export const edges = {
   1:{ source: "1", target: "2", edgeWidth: 1, hue: 200, cost: 10},
   2:{ source: "1", target: "3", edgeWidth: 1, hue: 200, cost: 20},
 
-  3:{ source: "2", target: "8", edgeWidth: 1, hue: 80, cost: 20 },
+  3:{ source: "2", target: "4", edgeWidth: 1, hue: 80, cost: 20 },
+  4:{ source: "2", target: "5", edgeWidth: 1, hue: 320, cost: 20 },
 
-  4:{ source: "3", target: "4", edgeWidth: 1, hue: 320, cost: 20},
-  5:{ source: "3", target: "5", edgeWidth: 1, hue: 320, cost: 20 },
-  6:{ source: "3", target: "7", edgeWidth: 1, hue: 320, cost: 20},
+  5:{ source: "3", target: "4", edgeWidth: 1, hue: 160, cost: 20 },
+  6:{ source: "3", target: "7", edgeWidth: 1, hue: 320, cost: 20 },
 
-  7:{ source: "2", target: "6", edgeWidth: 1, hue: 160, cost: 20 },
-  //{ source: "4", target: "7", edgeWidth: 1, hue: 160, cost: 20 },
-  8:{ source: "4", target: "2", edgeWidth: 1, hue: 160, cost: 20 },
+  7:{ source: "4", target: "6", edgeWidth: 1, hue: 80, cost: 20 },
+  8:{ source: "4", target: "7", edgeWidth: 1, hue: 160, cost: 20 },
 
-  9:{ source: "5", target: "6", edgeWidth: 1, hue: 80, cost: 20 },
-  10:{ source: "5", target: "7", edgeWidth: 1, hue: 80, cost: 20 },
-  11:{ source: "8", target: "7", edgeWidth: 1, hue: 80, cost: 20 }
+  9:{ source: "5", target: "6", edgeWidth: 1, hue: 160, cost: 20 },
+
+  10:{ source: "6", target: "8", edgeWidth: 1, hue: 80, cost: 20 },
+
+  11:{ source: "7", target: "8", edgeWidth: 1, hue: 200, cost: 20 }
 };
